d9682c85ea30124fa87f0986f2b49c2011646f82
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2023-11-24 20:54:06 +0100

    Add files via upload

--- Myprintf/ft_convert.c
+++ Myprintf/ft_convert.c
@@ -6,52 +6,46 @@
 /*   By: rshaheen <rshaheen@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/15 13:47:20 by rshaheen      #+#    #+#                 */
-/*   Updated: 2023/11/22 16:33:21 by rshaheen      ########   odam.nl         */
+/*   Updated: 2023/11/24 19:23:10 by rshaheen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "ft_printf.h"
 #include <stdlib.h>
 #include <string.h>
 
-char	*ft_convert(unsigned long n, char *base)
+char	*ft_convert_helper(unsigned long n, char *base, char *hex, int len)
 {
 	char	*str;
-	char	*hex;
-
-	hex = "0123456789abcdef";
-	if (n == 0)
-	{
-		str = (char *)malloc(2);
-		if (!str)
-			return (NULL);
-		str[0] = '0';
-		str[1] = '\0';
-		return (str);
-	}
-
-	int len = 0;
-	unsigned long temp = n;
-
-	while (temp > 0)
-	{
-		temp /= ft_strlen(base);
-		len++;
-	}
 
 	str = (char *)malloc(len + 1);
 	if (!str)
 		return (NULL);
-
 	str[len] = '\0';
-
 	while (n > 0)
 	{
 		len--;
-		str[len] = hex[n % ft_strlen(base)];
+		if (ft_strncmp(base, HEX_UPP_BASE, 16) == 0)
+			str[len] = HEX_UPP_BASE[n % ft_strlen(base)];
+		else
+			str[len] = hex[n % ft_strlen(base)];
 		n /= ft_strlen(base);
 	}
-
 	return (str);
+}
+
+char	*ft_convert(unsigned long n, char *base)
+{
+	char			*hex;
+	int				len;
+	unsigned long	temp;
+
+	hex = "0123456789abcdef";
+	if (n == 0)
+		return (strdup("0"));
+	len = 0;
+	temp = n;
+	while (temp > 0 && ++len)
+		temp /= ft_strlen(base);
+	return (ft_convert_helper(n, base, hex, len));
 }
